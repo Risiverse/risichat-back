52e071c62d19b86907d7a35457a2c5358e78ef0b
Florian Sylvain <floriansylvainpro@gmail.com>
2021-11-23 23:09:46 +0100

    🚧 Adding temporary client side UI for test purpose

---
+++ client/app.js
@@ -0,0 +1,31 @@
+let socket = new WebSocket("ws://127.0.0.1:8080/");
+console.log("Attempting Connection...");
+
+socket.onopen = () => {
+    console.log("Successfully Connected");
+};
+
+socket.onclose = event => {
+    console.log("Socket Closed Connection: ", event);
+};
+
+socket.onerror = error => {
+    console.error("Socket Error: ", error);
+};
+
+socket.onmessage = message => {
+    let p = document.createElement('p')
+    let newMessage = JSON.parse(message.data)
+    console.log(newMessage)
+    p.innerHTML = "<strong>" + newMessage['Username'] + "</strong><br>" + newMessage["Content"]
+    document.querySelector('div').appendChild(p)
+};
+
+let messageInput = document.querySelector('#message')
+let nicknameInput = document.querySelector('#nickname')
+
+document.querySelector("form").addEventListener('submit', e => {
+    e.preventDefault()
+    socket.send(`{ "Username" : "${nicknameInput.value}", "Content" : "${messageInput.value}" }`)
+    messageInput.value = ""
+})
