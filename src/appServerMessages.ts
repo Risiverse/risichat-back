5eeed887ff614d7f896525c26d8d74026dbc3d79
Florian Sylvain <floriansylvainpro@gmail.com>
2022-06-09 17:37:55 +0200

    Message not validated returns error | Refactor

--- src/appServerMessages.ts
+++ src/appServerMessages.ts
@@ -2,11 +2,16 @@ import { broadcastToAllClientsExceptSender } from './appServer'
 import { insertMessageIntoDB } from './appDatabase'
 import type { RawData, WebSocket } from 'ws'
 
-
-interface Message {
+interface chatMessage {
     timestamp: number,
     username: string,
     content: string
+}
+
+interface Message {
+    userSSOID: number,
+    type: string,
+    data: chatMessage
 }
 
 
@@ -21,12 +26,11 @@ export function escapeUnsafeMessageData(messageData: string): string {
 }
 
 
-function messageParser(messageData: RawData): string {
-    const receivedJSON: Message = JSON.parse(messageData.toString())
-    const parsedJSON: Message = {
-        timestamp: receivedJSON.timestamp,
-        username: escapeUnsafeMessageData(receivedJSON.username),
-        content: escapeUnsafeMessageData(receivedJSON.content)
+function chatMessageParser(messageData: chatMessage): string {
+    const parsedJSON: chatMessage = {
+        timestamp: messageData.timestamp,
+        username: escapeUnsafeMessageData(messageData.username),
+        content: escapeUnsafeMessageData(messageData.content)
     }
     return JSON.stringify(parsedJSON)
 }
@@ -45,24 +49,40 @@ function isNumberValid(field: number|null): boolean {
 }
 
 
-function isMessageValid(message: Message): boolean {
+function isChatMessageValid(message: chatMessage): boolean {
     return isStringValid(message.content) &&
         isStringValid(message.username) &&
         isNumberValid(message.timestamp)
 }
 
 
-export function messageHandler(messageData: RawData, websocket: WebSocket): void {
-    const messageDataJSON = JSON.parse(messageData.toString())
+function chatMessageHandler(messageData: any, senderWS: WebSocket): void {
+    if (isChatMessageValid(messageData)) {
+        const validParsedMessage = chatMessageParser(messageData)
+        broadcastToAllClientsExceptSender(validParsedMessage, senderWS)
+        insertMessageIntoDB(validParsedMessage)
+        console.log('Valid message')
+    } else {
+        senderWS.send(JSON.stringify({
+            status: 400,
+            message: 'The message could not be validated.',
+            data: messageData
+        }))
+        console.log('Invalid message')
+    }
+}
+
+
+const messagesTypesHandlers = [
+    { type: 'chatMessage', handler: chatMessageHandler }
+]
+
+
+export function messageHandler(messageData: RawData, senderWS: WebSocket): void {
+    const messageDataJSON: Message = JSON.parse(messageData.toString())
 
     console.log(messageDataJSON)
 
-    if (isMessageValid(messageDataJSON)) {
-        const validParsedMessage = messageParser(messageData)
-        broadcastToAllClientsExceptSender(validParsedMessage, websocket)
-        insertMessageIntoDB(validParsedMessage)
-        console.log('Message valid')
-    } else {
-        console.log('Message invalid')
-    }
+    messagesTypesHandlers.find(handlers => handlers.type === messageDataJSON.type)
+        ?.handler(messageDataJSON.data, senderWS)
 }
