3944abba1bb572ccecb7b1ab14412328c1ccc379
Florian Sylvain <floriansylvainpro@gmail.com>
2022-06-09 03:06:22 +0200

    Changed logging order

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,12 @@ import { WebSocketServer } from 'ws'
 const host: string = process.env['WS_HOST'] ? process.env['WS_HOST'] : 'locahost'
 const port: number = process.env['WS_PORT'] ? parseInt(process.env['WS_PORT']) : 9999
 
+console.log('Starting WS server...')
 
 export const WS = new WebSocketServer({
     host,
     port,
 })
-
-console.log('Starting WS server...')
 
 WS.on('listening', serverListeningHandler)
 
